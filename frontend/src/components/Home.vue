1cef521661f01862911e2a48a2e9e0db647d5f02
RUiN-jiarun <1052707477@qq.com>
2022-05-01 13:45:01 +0800

    homepage

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
     <div id="home">
-        这是一款面向艺术创作的完整图像风格迁移系统。
-    </div>
+        这是一款面向艺术创作的完整图像风格迁移系统。<br>
+        我们旨在通过最少的学习成本，提供最多的用户选择，获得最佳的图像质量。
+        <el-image
+          :src="require('../assets/20220426.jpg')"
+          class="image_2"
+          style="border-radius: 3px"
+        ></el-image>
+        <div class="img_info_1" style="border-radius: 0 0 5px 5px">
+              <span style="color: white; letter-spacing: 6px">系统流程示意</span>
+            </div>
+        Repository: TBA
+</div>
 </template>
 
 <script>
@@ -36,6 +46,30 @@ div {
   /* display: flex; */
   min-width: 1300px;
 
+}
+
+.image_1 {
+  width: 275px;
+  height: 260px;
+  background: #ffffff;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.image_2 {
+  width: 1100px;
+  height: 600px;
+  background: #ffffff;
+  /* display: inline-block; */
+  /* float:left; */
+}
+
+.img_info_1 {
+  height: 40px;
+  width: 1100px;
+  margin-bottom: 10px;
+  text-align: center;
+  background-color: #21b3b9;
+  line-height: 30px;
 }
 
 </style>
